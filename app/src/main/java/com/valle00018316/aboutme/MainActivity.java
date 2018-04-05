dd0e72ca8715ffd3b180fa847fecc7405fba4780
Carlos Valle <carlos.momos@gmail.com>
2018-04-04 21:34:18 -0600

    aboutme fix

---
+++ app/src/main/java/com/valle00018316/aboutme/MainActivity.java
@@ -0,0 +1,13 @@
+package com.valle00018316.aboutme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
