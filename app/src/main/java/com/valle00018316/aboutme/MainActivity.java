7aefe49cb20d6d789dff8410835c0df5a8ee134e
Carlos Valle <carlos.momos@gmail.com>
2018-04-07 18:01:14 -0600

    about me con intent

--- app/src/main/java/com/valle00018316/aboutme/MainActivity.java
+++ app/src/main/java/com/valle00018316/aboutme/MainActivity.java
@@ -1,13 +1,29 @@
 package com.valle00018316.aboutme;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fab =findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,  getResources().getText(R.string.enviar));
+                sendIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.enviar)));
+            }
+        });
     }
 }
